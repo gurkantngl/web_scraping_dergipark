1d63ddc7d0cbf13d3f83b7d6e4250aba0f68b94f
Gurkan Tongel <gurkantngl@gmail.com>
2024-03-13 15:09:52 +0300

    elastic

--- scraping_django/search_article/views.py
+++ scraping_django/search_article/views.py
@@ -9,56 +9,50 @@ from pymongo import MongoClient
 from bson.json_util import dumps
 import time
 
+
 def run_scrapy(input_words):
     process = CrawlerProcess(get_project_settings())
     process.crawl(articles.ArticlesSpider, keyword=input_words)
     process.start()
     process.join()
 
+
 def search(request):
-    print("search çalıştı")
-    if request.method == 'POST':
-        input_words = request.POST.get('inputWords', '')
+    if request.method == "POST":
+        input_words = request.POST.get("inputWords", "")
 
         scrapy_process = Process(target=run_scrapy, args=(input_words,))
         scrapy_process.start()
         scrapy_process.join()
 
-        client = MongoClient('127.0.0.1',27017)
+        client = MongoClient("127.0.0.1", 27017)
         db = client.Yazlab2
         collection_name = db["Articles"]
-        es = Elasticsearch('http://localhost:9200')
-        
+        es = Elasticsearch("http://localhost:9200")
+
+        keywords = [doc['keyword'] for doc in collection_name.find({}, {'_id': 0, 'keyword': 1}) if 'keyword' in doc]
+
         for document in collection_name.find():
-            document_id = document.pop('_id')
-            es.index(index='index', id=document_id, body=dumps(document))
-
-
+            document_id = document.pop("_id")
+            es.index(index="index", id=document_id, body=dumps(document))
         time.sleep(1)
-        
-        result = es.search(index='index', body={
-            "query": {
-                "bool": {
-                    "filter": [
-                        {
-                            "term": {
-                                "keyword.keyword": input_words
-                            }
-                        }
-                    ]
-                }
-        } ,
-            "size": 10000
-        })
-        
-        articles = result['hits']['hits']
-        articles = [article['_source'] for article in articles]
-        print("length: ",len(articles))
-        
-        response = es.indices.delete(index='index', ignore=[400, 404])
-        print("response: ",response)
 
-        return render(request, 'article_page.html', {'articles': articles})
-    
+        result = es.search(
+            index="index",
+            body={
+                "query": {
+                    "bool": {"filter": [{"term": {"keyword.keyword": input_words}}]}
+                },
+                "size": 10000,
+            },
+        )
+
+        articles = result["hits"]["hits"]
+        articles = [article["_source"] for article in articles]
+
+        response = es.indices.delete(index="index", ignore=[400, 404])
+
+        return render(request, "article_page.html", {"articles": articles, "keywords": keywords})
+
     else:
         return HttpResponse("Error")
